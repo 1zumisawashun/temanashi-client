0576a1f2cdb6697cadb6befab29f0d91a527dbbb
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-10 11:01:44 +0900

    add-typescript-on-dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList";
 import "./Dashboard.css";
@@ -5,23 +6,26 @@ import ProjectFilter from "./ProjectFilter";
 import { useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 
+// export default function Dashboard() {
 export default function Dashboard() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("projects");
-  const [currentFilter, setCurrentFilter] = useState("all");
+  const { documents, error }: { documents: any; error: any } = useCollection(
+    "projects"
+  );
+  const [currentFilter, setCurrentFilter] = useState<String>("all");
 
-  const changeFilter = (newFilter) => {
+  const changeFilter = (newFilter: any) => {
     setCurrentFilter(newFilter);
   };
 
   const projects = documents
-    ? documents.filter((document) => {
+    ? documents.filter((document: any) => {
         switch (currentFilter) {
           case "all":
             return true;
           case "mine":
             let assignedTome = false;
-            document.assignedUsersList.forEach((u) => {
+            document.assignedUsersList.forEach((u: any) => {
               if (user.uid === u.id) {
                 assignedTome = true;
               }
@@ -38,6 +42,7 @@ export default function Dashboard() {
         }
       })
     : null;
+
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
