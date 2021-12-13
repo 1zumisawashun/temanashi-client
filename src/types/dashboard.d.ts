f8d756505eaebbc6f88e4e220b33730e1fa0c461
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-13 20:57:25 +0900

    add-ts-difinision-file

--- src/types/dashboard.d.ts
+++ src/types/dashboard.d.ts
@@ -3,32 +3,57 @@ export type UseCollection = {
   error: String;
 };
 
-export type TimeStamp = {
-  nanoseconds: number;
-  seconds: number;
-};
+// export type TimeStamp = {
+//   nanoseconds: number;
+//   seconds: number;
+// };
 
 export type CreatedBy = {
   displayName: String;
-  id: String;
-  photoURL: String;
+  id: string;
+  online?: boolean;
+  photoURL: string;
 };
 
 export type Comments = {
   content: String;
   createdAt: TimeStamp;
   displayName: String;
-  id: Number;
+  id: number;
+  photoURL?: string;
+};
+
+export type Comment = {
+  content: String;
+  createdAt: Date;
+  displayName: String;
+  id: number;
+  photoURL?: string;
+};
+
+// FIXME:Documentに合わせてPickする
+export type Project = {
+  assignedUsersList: Array<CreatedBy>;
+  category: String;
+  //Commentが異なる
+  comments: Array<Comment>;
+  createdAt: any;
+  createdBy: CreatedBy;
+  details: String;
+  dueDate: any;
+  id: string;
+  name: String;
 };
 
 export type Document = {
   assignedUsersList: Array<CreatedBy>;
   category: String;
+  //Commentsが異なる
   comments: Array<Comments>;
   createdAt: TimeStamp;
   createdBy: CreatedBy;
   details: String;
   dueDate: TimeStamp;
-  id: String;
+  id: string;
   name: String;
 };
