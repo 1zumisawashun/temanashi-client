162ad27697a3b222abae6b8f7302cb7b042bed9b
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-13 20:55:03 +0900

    convert-ts-on-onlineuser

--- src/components/OnlineUsers.tsx
+++ src/components/OnlineUsers.tsx
@@ -1,15 +1,17 @@
 import "./OnlineUsers.css";
 import { useCollection } from "../hooks/useCollection";
 import Avatar from "../components/Avatar";
+import { CreatedBy } from "../types/dashboard";
 
-export default function OnlineUsers() {
+const OnlineUsers: React.FC = () => {
   const { error, documents } = useCollection("users");
+  console.log(documents);
   return (
     <div className="user-list">
       <h2>All Users</h2>
       {error && <div className="error">{error}</div>}
       {documents &&
-        documents.map((user) => (
+        documents.map((user: CreatedBy) => (
           <div key={user.id} className="user-list-item">
             {user.online && <span className="online-user"></span>}
             <span>{user.displayName}</span>
@@ -18,4 +20,5 @@ export default function OnlineUsers() {
         ))}
     </div>
   );
-}
+};
+export default OnlineUsers;
