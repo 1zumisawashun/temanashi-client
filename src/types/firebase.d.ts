376aef0a86c7b21d775bdcfdcf85fb30b7a94f84
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-18 00:30:13 +0900

    add-firebase-types

---
+++ src/types/firebase.d.ts
@@ -0,0 +1,13 @@
+type WhereFilterOp =
+  | "<"
+  | "<="
+  | "=="
+  | "!="
+  | ">="
+  | ">"
+  | "array-contains"
+  | "in"
+  | "array-contains-any"
+  | "not-in";
+
+type OrderByDirection = "desc" | "asc";
