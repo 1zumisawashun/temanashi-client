9f74d351fb5d53562c91eada5e0c024d734ce881
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-19 17:44:14 +0900

    convert-types-name

--- src/components/OnlineUsers.tsx
+++ src/components/OnlineUsers.tsx
@@ -1,17 +1,17 @@
 import "./OnlineUsers.css";
 import { useCollection } from "../hooks/useCollection";
 import Avatar from "../components/Avatar";
-import { CreatedUser } from "../types/dashboard";
+import { User } from "../types/dashboard";
 import { FC } from "react";
 
 const OnlineUsers: FC = () => {
-  const { error, documents } = useCollection<CreatedUser>("users");
+  const { error, documents } = useCollection<User>("users");
   return (
     <div className="user-list">
       <h2>All Users</h2>
       {error && <div className="error">{error}</div>}
       {documents &&
-        documents.map((user: CreatedUser) => (
+        documents.map((user: User) => (
           <div key={user.id} className="user-list-item">
             {user.online && <span className="online-user"></span>}
             <span>{user.displayName}</span>
