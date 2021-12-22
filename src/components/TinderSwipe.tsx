49c95416f6d7f3d4b51631d8e2b2fc838246ff96
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-22 21:54:12 +0900

    add-diagnose-button

--- src/components/TinderSwipe.tsx
+++ src/components/TinderSwipe.tsx
@@ -1,8 +1,11 @@
 import "./TinderSwipe.scss";
-import { FC, useState } from "react";
+import React, { FC, useState, useRef, useMemo } from "react";
 // import TinderCard from '../react-tinder-card/index'
 import TinderCard from "react-tinder-card";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
+import ThumbUp from "../assets/icon/thumb_up.svg";
+import ThumbDown from "../assets/icon/thumb_down.svg";
+import Undo from "../assets/icon/undo.svg";
 
 const db = [
   {
@@ -33,9 +36,28 @@ const db = [
 ];
 
 const TinderSwipe: FC = () => {
-  const history = useHistory();
-  const characters = db;
+  // const history = useHistory();
   const [lastDirection, setLastDirection] = useState<string>();
+  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1);
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo<any>(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val: any) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < db.length - 1;
+
+  const canSwipe = currentIndex >= 0;
 
   const swipedRight = () => {
     console.log("you swiped right");
@@ -44,10 +66,10 @@ const TinderSwipe: FC = () => {
     console.log("you swiped left");
   };
 
-  // NOTE:directionはup, downなど複数存在する
-  const swiped = (direction: string, nameToDelete: string) => {
-    console.log("removing: " + nameToDelete);
+  // set last direction and decrease current index
+  const swiped = (direction: string, nameToDelete: string, index: number) => {
     setLastDirection(direction);
+    updateCurrentIndex(index - 1);
     if (direction === "right") {
       swipedRight();
     }
@@ -56,25 +78,43 @@ const TinderSwipe: FC = () => {
     }
     // 最後までスワイプされたら一旦トップページにリダイレクトさせる
     // countメソッドありそう
-    if (nameToDelete === "Richard Hendricks") {
-      history.push("/");
-    }
+    // if (nameToDelete === "Richard Hendricks") {
+    //   history.push("/");
+    // }
   };
 
-  const outOfFrame = (name: string) => {
-    console.log(name + " left the screen!");
+  const outOfFrame = (name: string, idx: number) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir: string) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
   };
 
   return (
     <div className="tinder-swipe">
-      <h1 className="headline">React Tinder Card</h1>
       <div className="cardContainer">
-        {characters.map((character) => (
+        {db.map((character, index) => (
           <TinderCard
+            ref={childRefs[index]}
             className="swipe"
             key={character.name}
-            onSwipe={(dir) => swiped(dir, character.name)}
-            onCardLeftScreen={() => outOfFrame(character.name)}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
           >
             <div
               style={{ backgroundImage: "url(" + character.url + ")" }}
@@ -85,8 +125,21 @@ const TinderSwipe: FC = () => {
           </TinderCard>
         ))}
       </div>
+      <div className="buttons">
+        <button onClick={() => swipe("left")}>
+          <img src={ThumbDown} alt="" />
+        </button>
+        <button onClick={() => goBack()}>
+          <img src={Undo} alt="" />
+        </button>
+        <button onClick={() => swipe("right")}>
+          <img src={ThumbUp} alt="" />
+        </button>
+      </div>
       {lastDirection && (
-        <h2 className="infoText">You swiped {lastDirection}</h2>
+        <h3 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h3>
       )}
     </div>
   );
