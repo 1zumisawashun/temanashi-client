dd99ce95bd9dec33a15362394662cfd8f070883f
1zumisawashun <shun.1zumisawa@gmail.com>
2021-12-26 14:58:44 +0900

    convert-ts

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,67 @@
+import React, { createContext, useReducer, useEffect, FC } from "react";
+import { firebase, projectAuth } from "../firebase/config";
+
+type Action = {
+  type: string;
+  payload: firebase.User | null;
+};
+
+type State = {
+  user: firebase.User | null;
+  authIsReady?: boolean;
+};
+
+// authIsReadyはdispatchでのみトグルしている
+interface ContextInterface {
+  dispatch: React.Dispatch<Action>; // dispatch: (value: Action) => void;
+  user: firebase.User | null;
+  authIsReady?: boolean;
+}
+
+interface Props {
+  children: ChildNode;
+}
+
+const initState: State = {
+  user: null,
+  authIsReady: false,
+};
+
+export const AuthContext = createContext<ContextInterface | undefined>(
+  undefined
+);
+// 初期値undefinedだがuseAuthContextで型ガードしている
+
+export const authReducer: React.Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { user: action.payload, authIsReady: true };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initState);
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      if (!user) return;
+      dispatch({
+        type: "AUTH_IS_READY",
+        payload: user,
+      });
+      unsub();
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
