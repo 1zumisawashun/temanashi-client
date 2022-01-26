491c99650ac5ecccffff31900746e9c5056e3545
1zumisawashun <shun.1zumisawa@gmail.com>
2022-01-26 11:04:56 +0900

    stripeでポストできない修正

--- functions/src/controllers/stripeController.ts
+++ functions/src/controllers/stripeController.ts
@@ -2,33 +2,37 @@ import Stripe from "stripe";
 import { STRIPE_API } from "../env";
 import * as functions from "firebase-functions";
 
+const cors = require("cors");
 const stripe = new Stripe(STRIPE_API, { apiVersion: "2020-08-27" });
 
-const stripe_post = async (req: any, res: any) => {
+const stripe_post = (req: any, res: any) => {
   functions.logger.info(req.body);
-  const { photos, name, price, description, ...body } = req.body;
-  const createdAt = new Date().getTime() / 1000; // FIXME:timestampを追加する
-  try {
-    const product = await stripe.products.create({
-      name: name,
-      description,
-      active: true, // falseにするとstripeのダッシュボードの商品タブのアーカイブに入るので表示されない状態になる
-      images: photos, // 2MB未満の画像をアップロードしてくださいとのことでこれを満たさないと画像がアップロードされない
-      metadata: {
-        ...body, // NOTE:packageDimentionにwidthなどをまとめられる
-        createdAt,
-      },
-    });
-    await stripe.prices.create({
-      unit_amount: price,
-      currency: "jpy",
-      product: product.id,
-    });
-  } catch (error) {
-    return res.status(200).json({ status: req.body });
-  } finally {
-    return res.status(200).json({ status: req.body });
-  }
+  cors()(req, res, async () => {
+    // cors解除のためにラップする
+    const { photos, name, price, description, ...body } = req.body;
+    const createdAt = new Date().getTime() / 1000; // FIXME:timestampを追加する
+    try {
+      const product = await stripe.products.create({
+        name: name,
+        description,
+        active: true, // falseにするとstripeのダッシュボードの商品タブのアーカイブに入るので表示されない状態になる
+        images: photos, // 2MB未満の画像をアップロードしてくださいとのことでこれを満たさないと画像がアップロードされない
+        metadata: {
+          ...body, // NOTE:packageDimentionにwidthなどをまとめられる
+          createdAt,
+        },
+      });
+      await stripe.prices.create({
+        unit_amount: price,
+        currency: "jpy",
+        product: product.id,
+      });
+    } catch (error) {
+      return res.status(200).json({ status: req.body });
+    } finally {
+      return res.status(200).json({ status: req.body });
+    }
+  });
 };
 
 module.exports = {
