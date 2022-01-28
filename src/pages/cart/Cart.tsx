9c4139c5dcc88f3716f30b336c50c231314540cc
1zumisawashun <shun.1zumisawa@gmail.com>
2022-01-28 21:33:47 +0900

    複数購入の導線を確保

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,41 +1,96 @@
-import { FC } from "react";
-import { projectFunctions } from "../../firebase/config";
-import axios from "axios";
+import { FC, useState } from "react";
+import { line_item, productUseCase } from "../../utilities/stripeClient";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCartDocument } from "../../hooks/useCartDocument";
+import { Link } from "react-router-dom";
+import { taxIncludedPrice } from "../../utilities/convertValue";
+import Loading from "../../components/Loading";
 
 const Cart: FC = () => {
-  const onCallTest = () => {
-    const helloOnCall = projectFunctions.httpsCallable("helloOnCall");
-    helloOnCall({ name: `shun` }).then((result) => {
-      console.log(result.data);
-    });
+  // NOTE:sessionStorageがないとエラーになるのでハンドリングを実装する
+  const [isPendingBuy, setIsPendingBuy] = useState<boolean>(false);
+  const [line_items, setLinetems] = useState<Array<line_item>>([]);
+
+  const { user } = useAuthContext();
+  if (!user) throw new Error("we cant find your account");
+
+  const { documents } = useCartDocument();
+
+  const onClickBuy = async () => {
+    try {
+      setIsPendingBuy(true);
+      const uid = user.uid;
+      const seccess_url = `${window.location.origin}/complete`;
+      const cancel_url = `${window.location.origin}/error`;
+      await productUseCase.buy(uid, line_items, seccess_url, cancel_url);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(`Error: ${!!error.message ? error.message : error}`);
+      }
+    } finally {
+      setIsPendingBuy(false);
+    }
   };
-  const onRequestTest = async () => {
-    const url = "https://us-central1-temanashi-39b3f.cloudfunctions.net";
-    if (!url) return;
-    const result = await axios.get(`${url}/helloOnRequest`);
-    console.log(result, "result");
-  };
-  const getAxiosTest = async () => {
-    const url = "https://us-central1-temanashi-39b3f.cloudfunctions.net";
-    if (!url) return;
-    const result = await axios.get(`${url}/api/hello`);
-    console.log(result, "result");
+  const onClickSelect = async (price: string, quantity: number) => {
+    const lineItem: line_item = {
+      price,
+      quantity,
+    };
+    const newArray = [...line_items, lineItem];
+    await setLinetems(newArray);
   };
 
   return (
     <>
-      <div className="cart-container">
-        <div className="inner">
-          <button onClick={onCallTest} className="btn">
-            OnCallTest
-          </button>
-          <button onClick={onRequestTest} className="btn">
-            OnRequestTest
-          </button>
-          <button onClick={getAxiosTest} className="btn">
-            GetAxiosTest
-          </button>
-        </div>
+      {isPendingBuy && <Loading />}
+      <button className="btn" onClick={onClickBuy}>
+        購入する
+      </button>
+      <div className="diagnose-result-list">
+        {documents &&
+          documents.map((item: any) => (
+            <>
+              <Link to={`/furnitures/${item.product.id}`} key={item.product.id}>
+                <div className="image-box">
+                  {item.product.images.length > 0 ? (
+                    <img src={item.product.images[0]} alt="" />
+                  ) : (
+                    <img
+                      src="https://placehold.jp/200x160.png"
+                      alt=""
+                      width="100"
+                    />
+                  )}
+                </div>
+                <div className="content-box">
+                  <h4>{item.product.name}</h4>
+                  {Object.keys(item.prices).map((priceIndex) => (
+                    <p key={priceIndex}>
+                      {taxIncludedPrice(item.prices[priceIndex].unit_amount)}
+                    </p>
+                  ))}
+                  <div className="assigned-to">
+                    <ul>
+                      <li>幅111cm</li>
+                      <li>深さ222cm</li>
+                      <li>高さ333cm</li>
+                    </ul>
+                  </div>
+                </div>
+              </Link>
+              {Object.keys(item.prices).map((priceIndex) => (
+                <div key={priceIndex}>
+                  <button
+                    className="btn"
+                    onClick={() => onClickSelect(priceIndex, 1)}
+                  >
+                    選択する
+                  </button>
+                  <button className="btn">削除</button>
+                </div>
+              ))}
+            </>
+          ))}
       </div>
     </>
   );
