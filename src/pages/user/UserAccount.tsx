d04c7b1fc8207febbc8e1dd8aeb47d0efc3fa193
1zumisawashun <shun.1zumisawa@gmail.com>
2022-01-28 21:30:38 +0900

    testのためのcloud-functionsを移動ver2

--- src/pages/user/UserAccount.tsx
+++ src/pages/user/UserAccount.tsx
@@ -1,13 +1,41 @@
 import { FC } from "react";
 import UserNavbar from "../../components/UserNavbar";
+import { projectFunctions } from "../../firebase/config";
+import axios from "axios";
 
 const UserAccount: FC = () => {
+  const onCallTest = () => {
+    const helloOnCall = projectFunctions.httpsCallable("helloOnCall");
+    helloOnCall({ name: `shun` }).then((result) => {
+      console.log(result.data);
+    });
+  };
+  const onRequestTest = async () => {
+    const url = "https://us-central1-temanashi-39b3f.cloudfunctions.net";
+    if (!url) return;
+    const result = await axios.get(`${url}/helloOnRequest`);
+    console.log(result, "result");
+  };
+  const getAxiosTest = async () => {
+    const url = "https://us-central1-temanashi-39b3f.cloudfunctions.net";
+    if (!url) return;
+    const result = await axios.get(`${url}/api/hello`);
+    console.log(result, "result");
+  };
   return (
     <>
       <UserNavbar />
-      <div className="user-container">
+      <div className="cart-container">
         <div className="inner">
-          <p>account</p>
+          <button onClick={onCallTest} className="btn">
+            OnCallTest
+          </button>
+          <button onClick={onRequestTest} className="btn">
+            OnRequestTest
+          </button>
+          <button onClick={getAxiosTest} className="btn">
+            GetAxiosTest
+          </button>
         </div>
       </div>
     </>
