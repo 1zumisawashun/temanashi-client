531ab66ba1eea903a00c3f093ba57352cf846ac4
1zumisawashun <shun.1zumisawa@gmail.com>
2022-02-03 12:08:49 +0900

    利用規約プラポリのダミー追加

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,11 +1,64 @@
 import { FC } from "react";
+import Divider from "../components/Divider";
 
 const privacy: FC = () => {
   return (
     <>
-      <div className="user-container">
+      <div className="terms">
         <div className="inner">
-          <p>Privacy</p>
+          <p className="title">お客さまの個人情報のお取扱いについて</p>
+          <p className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+            tempor incidunt ut labore et dolore magna aliqua.
+          </p>
+          <Divider />
+          <div className="box">
+            <p className="article">第1条項</p>
+            <p className="text">
+              Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+              tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute
+              iure reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint obcaecat cupiditat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <Divider />
+          <div className="box">
+            <p className="article">第2条項</p>
+            <p className="text">
+              Lorem ipsum dolor sit amet. Et dolorem laboriosam est explicabo
+              excepturi est molestiae quam. Qui magni galisum in nesciunt animi
+              aut commodi galisum et nisi temporibus ad cumque quaerat est quia
+              quibusdam. Sed dolore sint est enim nesciunt sed repellendus
+              dolores! Ut ipsa soluta et ducimus tempora qui similique harum qui
+              eligendi repellat.
+            </p>
+          </div>
+          <Divider />
+          <div className="box">
+            <p className="article">第3条項</p>
+            <p className="text">
+              Cum ullam sint et veniam internos rem quasi magnam. Non nemo odio
+              aut voluptatem quia rem unde optio. Et corrupti sapiente hic quia
+              molestias est numquam maxime. Et consectetur omnis eos quae sint
+              aut sunt impedit cum quidem dolores qui asperiores magni.
+            </p>
+          </div>
+          <Divider />
+          <div className="box">
+            <p className="article">第4条項</p>
+            <p className="text">
+              Est saepe ipsum et aspernatur praesentium vel totam commodi quo
+              expedita vitae hic omnis assumenda est dolorem omnis. Cum atque
+              dolorem qui nisi consequuntur eum magni eveniet. Ex accusamus
+              animi sed rerum iste ad maxime quam ad natus voluptatum non
+              provident expedita non amet enim.
+            </p>
+          </div>
+          <Divider />
+          <div className="date">【制定日2022年2月3日】</div>
         </div>
       </div>
     </>
